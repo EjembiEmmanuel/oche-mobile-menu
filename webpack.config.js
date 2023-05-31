1e26ed5dad8c4c4ea2807dffba9cf74751dab9ac
EjembiEmmanuel <ejembioche50@gmail.com>
2023-05-31 11:38:47 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Mobile Menu",
+    }),
+  ],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    globalObject: "this",
+    library: {
+      name: "mobile",
+      type: "umd",
+    },
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
